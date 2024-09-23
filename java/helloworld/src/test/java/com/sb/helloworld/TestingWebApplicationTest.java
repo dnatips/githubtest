0c44e55c6b2be68aa4296fd8b6c63cf5c7befb52
dnatips <dna_tips@yahoo.co.in>
2024-09-23 17:39:15 +0530

    add more tests

---
+++ java/helloworld/src/test/java/com/sb/helloworld/TestingWebApplicationTest.java
@@ -0,0 +1,28 @@
+package com.sb.helloworld;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestingWebApplicationTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Hi there!")));
+    }
+}
