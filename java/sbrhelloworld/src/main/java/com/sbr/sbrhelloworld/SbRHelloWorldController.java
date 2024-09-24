7aa107048b01a0db99047797bca51e0b399839f9
dnatips <dna_tips@yahoo.co.in>
2024-09-24 22:21:14 +0530

    Add code to parally fetch 2 delayed calls & combine output to return in optimal time

--- java/sbrhelloworld/src/main/java/com/sbr/sbrhelloworld/SbRHelloWorldController.java
+++ java/sbrhelloworld/src/main/java/com/sbr/sbrhelloworld/SbRHelloWorldController.java
@@ -1,6 +1,6 @@
 package com.sbr.sbrhelloworld;
 
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.web.bind.annotation.*;
@@ -10,22 +10,24 @@ import reactor.core.publisher.*;
 public class SbRHelloWorldController {
     @GetMapping("hw")
     public Mono<String> greetingMessage() {
-        return computeMessage();
+        return getIntroMessage()
+            .zipWith(getFooterMessage())
+            .map((value) -> {
+                return value.getT1() + value.getT2();
+            });
     }
 
-    private Mono<String> computeMessage() {        
+    private Mono<String> getIntroMessage() {
         String introMessage = "Hi there! I am a REACTIVE SPRING BOOT App! How are you doing? <br />";
-        String message = introMessage + getFooterMessage();
-
-        return Mono.just(message);
+        return Mono.just(introMessage).delayElement(Duration.ofSeconds(5));
     }
 
-    private String getFooterMessage() {
+    private Mono<String> getFooterMessage() {
         String dateTimeFormat = "dd/MM/yyyy hh:mm:ss a";
         String dateTimeMessage = LocalDateTime.now().format(
             DateTimeFormatter.ofPattern(dateTimeFormat));
         String footerMessage = String.format("Controller Object ID - %d : Now - %s <br />"
         , this.hashCode(), dateTimeMessage);
-        return footerMessage;
+        return Mono.just(footerMessage).delayElement(Duration.ofSeconds(3));
     }
 }
