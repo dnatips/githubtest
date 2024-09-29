0281696fddeffa1b73e8d26cc97a79ebf3bb6988
dnatips <dna_tips@yahoo.co.in>
2024-09-29 16:05:43 +0530

    Add conditional statements and functions to JavaScript.

--- html-poc/app.js
+++ html-poc/app.js
@@ -1,3 +1,12 @@
 var message = "Hi there! What's going on?"
 console.log(message)
 console.log("Try testing HTML page with Javascript included in it through another .js file.")
+
+function calculateShippingCost(price) {
+    return (price <= 10) ? 5 : ((price <=20) ? 3 : 0);
+}
+
+let totalPrice = 19;
+let shippingCost = calculateShippingCost(totalPrice);
+console.log(`Shipping cost is ${shippingCost}.`);
+console.log(new Date().toString());
