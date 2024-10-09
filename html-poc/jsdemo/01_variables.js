66c41562fff9ae559b2d1d36229a4be9ad3da900
dnatips <dna_tips@yahoo.co.in>
2024-10-09 20:11:21 +0530

    Add variables Demo | Add Javascript Hello World Demo to run from Node.js for testing purpose

---
+++ html-poc/jsdemo/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "person@mailbox.com";
+var accountPassword = "12345";
+accountCity = "Solapur";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+// accountId = 2;
+accountEmail = "new@mailbox.com";
+accountPassword = "a1b2c3";
+accountCity = "Pune";
+console.log("After modifications...");
+console.table([accountId,accountEmail,accountPassword,accountCity]);
