218deb8c4da63f66b0b4c703a5386788a083c5a3
dnatips <dna_tips@yahoo.co.in>
2024-10-17 22:56:40 +0530

    Added a local React Tag for POC

--- html-poc/react-vite-demo/src/main.jsx
+++ html-poc/react-vite-demo/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+
+function GetCurrentDateTime(){
+  return (new Date()).toLocaleString();
+}
 
 createRoot(document.getElementById('root')).render(
   <>
     <App />
+    <GetCurrentDateTime />
   </>,
 )
+
+export default GetCurrentDateTime
