99e78cd051a3057bce001251886f057de1f3b3aa
dnatips <dna_tips@yahoo.co.in>
2024-10-18 18:04:21 +0530

    React Vite POC for Hooks. Update page automatically on state change.

---
+++ html-poc/react-vite-02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+  
+  // let counter = 5
+
+  const incrementValue = () => {
+    if(counter < 20){
+      setCounter(counter + 1);
+      console.log(`Value incremented. ${counter} ${Math.random()}`);
+    }
+  }
+
+  const decrementValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1);
+      console.log(`Value decremented. ${counter} ${Math.random()}`);
+    }
+  }
+
+  return (
+    <>
+      <h1>What's up, Vite React!!</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={incrementValue}>Increment value</button>
+      <br />
+      <button onClick={decrementValue}>Decrement value</button>
+    </>
+  )
+}
+
+export default App
