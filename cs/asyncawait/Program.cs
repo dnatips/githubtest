02c3bf09fa954ec472e4070d748ea2f856d22018
dnatips <dna_tips@yahoo.co.in>
2024-10-28 00:05:23 +0530

    C# Async Await POC

---
+++ cs/asyncawait/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace asyncawait;
+
+using System;
+using System.IO;
+using System.Threading;
+
+enum OperationStatus
+{
+    None,
+    NotStarted,
+    InProgress,
+    Cancelled,
+    Completed
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        OperationStatus operationStatus = OperationStatus.NotStarted;
+        Console.CancelKeyPress += delegate { 
+            operationStatus = OperationStatus.Cancelled;
+            Console.WriteLine();
+            Console.WriteLine("User cancelled the copy operation!!");
+        };
+        var filePath = "test.txt";
+        var fileInfo = new FileInfo(filePath);
+        Console.WriteLine("Size of {0} is {1}.", filePath, fileInfo.Length);
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        operationStatus = OperationStatus.InProgress;
+        Task<string> fileReadTask = File.ReadAllTextAsync(filePath, cancellationTokenSource.Token);
+        Console.WriteLine("Reading file async way...");
+        while(operationStatus == OperationStatus.InProgress)
+        {
+            if(fileReadTask.IsCompletedSuccessfully)
+            {
+                operationStatus = OperationStatus.Completed;
+            }
+            else
+            {
+                Console.Write(".");
+                Thread.Sleep(500);
+            }
+        }
+
+        if(operationStatus == OperationStatus.Completed)
+        {
+            string text = await fileReadTask;
+            Console.WriteLine();
+            Console.WriteLine(text.Length);
+        }
+    }
+}
