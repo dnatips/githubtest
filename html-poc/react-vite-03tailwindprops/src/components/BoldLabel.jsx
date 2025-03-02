d6ecdc77a809d8d169d6426e0a098585da679e8c
dnatips <dna_tips@yahoo.co.in>
2025-03-02 19:41:30 +0530

    Componentization of code

---
+++ html-poc/react-vite-03tailwindprops/src/components/BoldLabel.jsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+
+function BoldLabel(props) {
+    return (
+        <h1 className='bg-blue-900 text-white p-4 rounded-xl'>{props.message}</h1>
+    );
+};
+
+export default BoldLabel;
