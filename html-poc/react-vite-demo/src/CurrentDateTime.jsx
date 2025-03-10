26284d66aa63bd8e40ca4259b0461aaea20f4512
dnatips <dna_tips@yahoo.co.in>
2025-03-10 18:38:16 +0530

    Datetime component updated to keep updating time every second.

--- html-poc/react-vite-demo/src/CurrentDateTime.jsx
+++ html-poc/react-vite-demo/src/CurrentDateTime.jsx
@@ -1,8 +1,20 @@
+import { useState, useEffect } from 'react'
+
 export default function CurrentDateTime() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <h1>Current DateTime</h1>
-      <div>The time right now is {(new Date()).toLocaleString()}</div>
+      <div>The time right now is {time.toLocaleString()}</div>
     </>
   )
 }
